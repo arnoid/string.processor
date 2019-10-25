d5a9fddb2859ca381e67a0eba0891996ef603002
arnoid <stratoangel@gmail.com>
2019-10-25 17:39:37 +0200

    [ADD] DictionaryStringProvider
    - MutableMap-based string provider

---
+++ string_processor/src/main/kotlin/org/arnoid/string/processor/DictionaryStringProvider.kt
@@ -0,0 +1,18 @@
+package org.arnoid.string.processor
+
+class DictionaryStringProvider(
+        private val dictionary: MutableMap<String, String> = HashMap(),
+        private val emptyValue: String = ""
+) : StringProvider {
+    fun clear() {
+        dictionary.clear()
+    }
+
+    override fun get(key: String): String {
+        return dictionary[key] ?: emptyValue
+    }
+
+    override fun set(key: String, value: String) {
+        dictionary[key] = value
+    }
+}
